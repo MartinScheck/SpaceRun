4af1fdd8215182878c562797cf9f95e7a202f8ce
sta-alex <80824722+sta-alex@users.noreply.github.com>
2023-10-29 22:35:25 +0100

    Lazor

--- Assets/Scripts/LazorSkript.cs
+++ Assets/Scripts/LazorSkript.cs
@@ -5,25 +5,49 @@ using UnityEngine;
 public class LazorSkript : MonoBehaviour
 {
     public HeroScript hero;
-    private float time;
+    private float dmgdelaytime = 0.5f;
+    private bool dmgOn = false;
+    private float timeSinceLastDamage = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     { 
     }
+
+    private void Update()
+    {
+       
+    }
     private void FixedUpdate()
     {
-        
+        if (dmgOn)
+        {
+            timeSinceLastDamage += Time.deltaTime;
+            if (timeSinceLastDamage >= dmgdelaytime)
+            {
+                hero.decreaseHealth();
+                timeSinceLastDamage = 0.0f;
+            }
+        }
     }
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.CompareTag("Hero"))
+        {
+            dmgOn = true;
+        }
+    }
+  
+    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Hero"))
+        {
+            dmgOn = false;
+            timeSinceLastDamage = 0.0f;
+            hero.decreaseHealth();
 
-        time = time + Time.deltaTime;
-        Debug.Log(time);
-            
-        hero.decreaseHealth();
-        
-       
+        }
     }
 
 }
