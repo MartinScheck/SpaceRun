13c3ba03b73b8d957902f537e2780101d464ee38
sta-alex <80824722+sta-alex@users.noreply.github.com>
2023-10-31 00:56:10 +0100

    Gunmovement

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -16,9 +16,18 @@ public class GunScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 directionToTarget = hero.transform.position - gameObject.transform.position;
+        /*Vector3 directionToTarget = hero.transform.position - gameObject.transform.position;
         Quaternion targetRotation = Quaternion.LookRotation(directionToTarget);
-        gameObject.transform.rotation = targetRotation;
+        gameObject.transform.rotation = targetRotation;*/
+
+        // Berechne die Richtung vom sourceObject zum targetObject.
+        Vector3 directionToTarget = hero.transform.position - gameObject.transform.position;
+
+        // Berechne den Winkel zwischen der aktuellen Richtung und der Richtung zum Ziel.
+        float angle = Mathf.Atan2(directionToTarget.y, directionToTarget.x) * Mathf.Rad2Deg;
+
+        // Rotiere das GameObject in Richtung des Ziels.
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 
 
