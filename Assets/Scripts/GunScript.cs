02d16630de70aad571908faa10034aff44e30386
sta-alex <80824722+sta-alex@users.noreply.github.com>
2023-10-31 00:37:49 +0100

    GunRotation

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -4,57 +4,22 @@ using UnityEngine;
 
 public class GunScript : MonoBehaviour
 {
-    public float correctionPosition;
-    public float currentGunAngle;
-    private float differenz;
-    public float gunPosX;
-    public float gunPosY;
+    public GameObject hero;
+    
  
     // Start is called before the first frame update
     void Start()
     {
-        correctionPosition = 0.013f;
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-
-        if (currentGunAngle != getGunAngle())
-        {
-            gunPosX = gameObject.transform.position.x;
-            gunPosY = gameObject.transform.position.y;
-            differenz = Mathf.Abs(currentGunAngle) - Mathf.Abs(getGunAngle());
-            
-            if(differenz < 0)
-            {
-                gunPosX = gunPosX + (differenz * correctionPosition) ;
-                gunPosY =  gunPosY - (differenz * correctionPosition);
-                gameObject.transform.position = new Vector3(gunPosX, gunPosY, gameObject.transform.position.z);
-
-            }
-            else
-            {
-                gunPosX = gunPosX - (differenz * correctionPosition);
-                gunPosY = gunPosY + (differenz * correctionPosition);
-                gameObject.transform.position = new Vector3(gunPosX, gunPosY, gameObject.transform.position.z);
-            }
-            
-
-
-        }
-        currentGunAngle = getGunAngle();
+        Vector3 directionToTarget = hero.transform.position - gameObject.transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(directionToTarget);
+        gameObject.transform.rotation = targetRotation;
     }
 
-    public void RotateGun()
-    {
-        
-    }
 
-    public float getGunAngle()
-    {
-        return gameObject.transform.rotation.eulerAngles.z;
-    }
-    
 }
