dd104b9b66522c37dff57494d2c5eb406c4878fd
sta-alex <80824722+sta-alex@users.noreply.github.com>
2023-10-31 19:11:30 +0100

    HelpScene

---
+++ Assets/Scripts/HelpSceneScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HelpSceneScript : MonoBehaviour
+{
+    public GameObject backButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backButton.GetComponent<Button>().onClick.AddListener(BackButton);
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene("OptionScene");
+    }
+}
